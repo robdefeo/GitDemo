d7b0d360748e3e6fdd8b2d0a95150d457ff4d883
Luke De Feo <lukedefdeveloper@gmail.comfr>
2012-11-24 21:08:59 +0000

    First commit!

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Luke
+ * Date: 24/11/2012
+ * Time: 21:05
+ */
+public class Test {
+}
---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Luke
+ * Date: 24/11/2012
+ * Time: 21:05
+ */
+public class Test2 {
+    String anything = "the";
+    int a;
+
+    public Test2(String anything) {
+        this.anything = anything;
+        this.a = 2;
+    }
+}
